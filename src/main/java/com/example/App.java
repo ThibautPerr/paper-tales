e3da2332b591493b9ffd0594848747f3d470068b
Thibaut Perrouin <thibaut.perrouin@etu.univ-nantes.fr>
2024-02-14 12:17:07 +0100

    ✨ Added parameters to run

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -21,9 +21,9 @@ import com.example.utils.Utils;
 
 public class App {
     public static void main(String[] args) {
-        Utils.setStartLogs(false);
-        Utils.setEndLogs(true);
-        int gamesPlayed = 1;
+        int gamesPlayed = args.length > 0 ? Integer.parseInt(args[0]) : 1;
+        Utils.setStartLogs(args.length > 1 && args[1].equals("true"));
+        Utils.setEndLogs(args.length > 2 && args[2].equals("true"));
 
         List<Strategy> strategies = createStrategies();
 
